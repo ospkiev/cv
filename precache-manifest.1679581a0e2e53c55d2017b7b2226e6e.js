eaaef7dcd034a3723a9ab881d6a88306ef8b151f
Pavel Karpenko <ospkiev@gmail.com>
2019-02-11 19:08:13 +0200

    Updates

---
+++ precache-manifest.1679581a0e2e53c55d2017b7b2226e6e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "db463b2b30b9e7a212dc734f76b22056",
+    "url": "/cv/static/media/Фото2.db463b2b.jpg"
+  },
+  {
+    "revision": "2113c1ace128d01797bff5c04b2cbc62",
+    "url": "/cv/static/media/weather.2113c1ac.jpg"
+  },
+  {
+    "revision": "d8664a6d2f7c2a54e5f9e5166d733653",
+    "url": "/cv/static/media/site.d8664a6d.jpg"
+  },
+  {
+    "revision": "3f3a46b4b57a895575b908a675d4ed9d",
+    "url": "/cv/static/media/game.3f3a46b4.jpg"
+  },
+  {
+    "revision": "dd1027dd606f7962cce8bc15c0f640c0",
+    "url": "/cv/static/media/chat.dd1027dd.jpg"
+  },
+  {
+    "revision": "084ac45aee39707f976e",
+    "url": "/cv/static/js/runtime~main.084ac45a.js"
+  },
+  {
+    "revision": "6d68baeba6cdf1a141d6",
+    "url": "/cv/static/js/main.6d68baeb.chunk.js"
+  },
+  {
+    "revision": "92421859c6173829ddf0",
+    "url": "/cv/static/js/1.92421859.chunk.js"
+  },
+  {
+    "revision": "6d68baeba6cdf1a141d6",
+    "url": "/cv/static/css/main.28e5d646.chunk.css"
+  },
+  {
+    "revision": "059daa752dacceecd2dbc8302fca3faa",
+    "url": "/cv/index.html"
+  }
+];
