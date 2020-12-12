026337795ab341097632efc3c56174542f91f8f2
Pavel Karpenko <ospkiev@gmail.com>
2020-12-12 07:39:12 +0200

    add alt

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -6,17 +6,19 @@ const About = () => {
         <div className='about'>
             <p className='about__me'>My name is Pavel Karpenko, I'm  web-developer with a strong interest in creating Web App based on JS/React/Redux.
                 I'm always looking forward to learning new technologies and improving already learned ones.
-                I began with HTML5/CSS3 and JavaScript basics, and moved to React/Redux ,I made a couple of single page applications.
-                I expect a project with technologies such as React/Redux .
+                I began with HTML5/CSS3 and JavaScript basics, and moved to React/Redux ,
+                I have 7 monthes of experience in commercial projects,
+                worked in team via SCRUM with backend, PM, QA.
+                Technology I use in my work : JS,React,Redux,HTML5,CSS3,REST,Webpack,Material/Semantic,Photoshop,Figma,Gulp/SASS, Git,adaptive and responsive layout.
                 </p>
-            <p className='skills'> <span>Skills:</span> JavaScript, React, Redux, Semantic UI, CSS3, HTML5, SASS/SCSS, Gulp, Webpack, Git, Agile, Scrum.
+            <p className='skills'> <span>Skills:</span> JavaScript, React, Redux, Material/Semantic, CSS3, HTML5, SASS/Gulp, Webpack, Git, Agile, Scrum.
             </p>
             <p className='experience'> <span> Work experience.<br /></span>
                 1.Upscale Academy (Kyiv)<br />
-                Front-end developer, Sept2018 – Now.<br />
+                Front-end developer, Dec2018 – Now.<br />
                 Responsibilities : create web sites ,single page applications .<br />
                 2. GIMedia (Kyiv)<br />
-                Sales/support engineer, Jan 2017 – Aug 2018.<br />
+                Technical support engineer, Jan 2017 – Aug 2018.<br />
                 Responsibilities : provide software and hardware for Digital Signage Solutions
                 (manage and control video/audio content remotely in advertising industry).<br />
                 3.National Technical University of Ukraine 'Kyiv Polytechnic Institute' (Kyiv)<br />
